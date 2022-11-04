49690b4eda0d0e5c33c4d0c2d8838a20fbc02f40
Yaircitop <yair.cardenas@usm.cl>
2022-11-03 21:14:39 -0300

    calendario y statics

--- Qalendiary/views.py
+++ Qalendiary/views.py
@@ -12,7 +12,7 @@ def subirobjetivo(request):
         form = ObjetivoForm(request.POST)
         if form.is_valid():
             instance = form.save(commit=False)
-            instance.usuario = request.user.username
+            instance.usuario = request.user.id
             instance.save()
             return redirect("dashboard")
     else:
@@ -22,7 +22,7 @@ def subirobjetivo(request):
     })
 
 def verobjetivo(request):
-    lista_objetivo= Objetivo.objects.all()
+    lista_objetivo= Objetivo.objects.filter(usuario=request.user.id)
     return render(request, 'verobjetivo.html', {
         'lista_objetivo': lista_objetivo
     })
@@ -37,7 +37,8 @@ def registerView(request):
             form.save()
             return redirect('login_url')
         else:
-            form = NewRegister()
+            #form = NewRegister()
+            return render(request,'registration/register2.html',{'form':NewRegister})
     
     return render(request,'registration/register.html',{'form':NewRegister})
 
